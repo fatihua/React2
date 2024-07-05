37b657ba3a42670a8076da42da5ba5257afc291b
fatihua <fatihusameavseren@gmail.com>
2024-07-05 09:06:33 +0200

    05.07.2024 react

---
+++ class-notes/RC-03-react-bootstrap/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import MyNavbar from "./components/MyNavbar";
+import Slider from "./components/Slider";
+import MyCard from "./components/MyCard";
+import dataa from "./data"
+
+function App() {
+  return (
+    <div>
+      <MyNavbar/>
+      <Slider/>
+      <MyCard veri={dataa}/>
+    </div>
+  );
+}
+
+export default App;
