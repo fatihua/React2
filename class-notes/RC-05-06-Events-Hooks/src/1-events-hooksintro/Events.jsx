820da22f070150b04488abf23439a99f03f046cb
fatihua <fatihusameavseren@gmail.com>
2024-07-09 13:59:33 +0200

    rc05

---
+++ class-notes/RC-05-06-Events-Hooks/src/1-events-hooksintro/Events.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Events = () => {
+    //!js alanı
+
+let baslik = "MERHABA ARKADAŞLAR"
+let count = 0;
+
+const arttır =()=>{
+    count++;
+    console.log(count);
+    document.querySelector("span").textContent=count
+}
+
+const butonTitle =(title)=>{
+    console.log(title)
+    document.querySelector(".btn-danger").textContent=title
+}
+ //! react alanında parametreli fonksiyon çağıracaksak, fonksiyonun adının önüne ()=> işaretini koymalıyız, yoksa fonksiyonu event ı beklemeden çalıştırıyor
+
+ const baslikChange =(yeni)=>{
+    baslik=yeni
+    console.log(baslik);
+    document.querySelector("h1").textContent=baslik
+ }
+
+  return (
+    //!jsx/react alanı
+
+    <div className='text-center container'>
+        <h1>INFO : {baslik}</h1>
+        <h2>COUNT: <span className='text-danger'>{count}</span></h2>
+        <button onClick={arttır} className='btn btn-primary'>ARTTIR</button>
+        <button onClick={()=>butonTitle("ipek")} className='btn btn-danger m-2'>DEĞİŞTİR</button>
+        <button onClick={()=>baslikChange("hello friends")} className='btn btn-info'>TIKLANDI</button>
+    </div>
+  )
+}
+
+export default Events
+
+
+//?-------------------------------------------------------------------
+//! Konsolda, güncellenen değişen count u görebiliriz ancak web sayfasında (biz görüntüle demeden) görüntülenmiyor..
+//* Çünkü, REACT herhangi bir element i default olarak static sayar
+//*DOM manipülasyonunu en aza indirmek için bunu yapar
+//* Hangi elementin interactive olduğu konusunda React'e bilgi vermeliyiz.
+//! REACT'i interactive elements hakkında bilgilendirmek için iki yöntem kullanıyoruz
+//* 1. Statefull Classes. State-based inform (Class Components)
+//* 2. Hooks (Functional Components)
+//?-------------------------------------------------------------------- */
---
+++ class-notes/RC-05-06-Events-Hooks/src/App.js
@@ -0,0 +1,18 @@
+import Events from "./1-events-hooksintro/Events";
+import Hooks from "./2-hooks/Hooks";
+import Counter from "./3-classComponents/Counter";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css" 
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Events/>
+      <Hooks/> */}
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
