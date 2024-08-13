e416a438c1dde321157c1a155067f290c40f7cda
fatihua <fatihusameavseren@gmail.com>
2024-08-13 10:27:59 +0200

    styled-1 fertig

---
+++ class-notes/RC-16-Styled-Components/src/components/style/HeaderStyle.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const HeaderS = styled.h1`
+  color: red;
+  font-size: 2.5rem;
+  margin: 1rem 0;
+`;
+
+export const LinkS = styled.a`
+color: salmon;
+text-decoration: none;
+font-size: 2rem;
+
+
+`
+
+export default HeaderS;
