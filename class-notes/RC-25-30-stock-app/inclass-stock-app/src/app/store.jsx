c7ffdc103792b5a8660221b9e7b696a1d0191ab9
fatihua <fatihusameavseren@gmail.com>
2024-10-04 12:40:35 +0200

    inside pages start

--- class-notes/RC-25-30-stock-app/inclass-stock-app/src/app/store.jsx
+++ class-notes/RC-25-30-stock-app/inclass-stock-app/src/app/store.jsx
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 // import authMidReducer from "../features/authSliceMiddleware"; //!middleware ile olan kullanım
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+//* redux persist
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+//* redux toolkit store
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer, //* kalıcı hale gelmesini istediğimiz reducer'i store'a verdik
+    // auth: authReducer,
     // auth:authMidReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+//* redux persist
+export let persistor = persistStore(store)
 export default store;
