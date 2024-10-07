12af1d72c05e554ed5a22c02978cb39a075fbc35
fatihua <fatihusameavseren@gmail.com>
2024-10-07 23:06:58 +0200

    firmCard fortsetzen

--- class-notes/RC-25-30-stock-app/inclass-stock-app/src/components/Cards/FirmCard.jsx
+++ class-notes/RC-25-30-stock-app/inclass-stock-app/src/components/Cards/FirmCard.jsx
@@ -5,27 +5,35 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function FirmCard() {
+export default function FirmCard({_id, name, phone, address, image}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
+    <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {address}
+        </Typography>
+      </CardContent>
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{ height: 140, objectFit:"contain" }}
+        image={image}
+        component="img"
         title="green iguana"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+        <Typography variant="body2">
+          Phone: {phone}
         </Typography>
       </CardContent>
+      
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <EditIcon/>
+        <DeleteIcon/>
       </CardActions>
     </Card>
   );
